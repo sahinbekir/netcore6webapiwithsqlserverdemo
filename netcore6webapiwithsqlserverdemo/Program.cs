a250a5381ab74508ed4afe8199fcabd0c8d78469
schahinbekir <bsbekirsahinbs@gmail.com>
2022-09-26 19:10:17 +0300

    Added CORS.

--- netcore6webapiwithsqlserverdemo/Program.cs
+++ netcore6webapiwithsqlserverdemo/Program.cs
@@ -15,18 +15,38 @@ builder.Services.AddDbContext<DemoDbContext>(
 /*builder.Services.AddDbContext<DemoDbContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("Npgsql")));*/
 
+
+var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, builder => {
+        //builder.WithOrigins("http://localhost:800").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        //builder.SetIsOriginAllowed(origin => true);
+    });
+});
+
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(devCorsPolicy);
+
 }
 
 app.UseHttpsRedirection();
 
+
 app.UseAuthorization();
+//app.UseCors(prodCorsPolicy);
 
 app.MapControllers();
 
