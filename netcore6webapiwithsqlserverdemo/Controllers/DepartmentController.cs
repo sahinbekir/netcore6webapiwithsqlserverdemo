642043b309d79cd0e77c446f049749ec041ef76a
schahinbekir <bsbekirsahinbs@gmail.com>
2022-09-26 18:21:08 +0300

    .net core web api commit.

---
+++ netcore6webapiwithsqlserverdemo/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using netcore6webapiwithsqlserverdemo.DB;
+using netcore6webapiwithsqlserverdemo.Models;
+
+namespace netcore6webapiwithsqlserverdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly DemoDbContext _demoDbContext;
+        public DepartmentController(DemoDbContext demoDbContext)
+        {
+            _demoDbContext = demoDbContext;
+        }
+        //List
+        [HttpGet]
+        public async Task<IEnumerable<Department>> Get()
+
+            => await _demoDbContext.Departments.ToListAsync();
+    }
+}
---
+++ netcore6webapiwithsqlserverdemo/Controllers/JobController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using netcore6webapiwithsqlserverdemo.DB;
+using netcore6webapiwithsqlserverdemo.Models;
+
+namespace netcore6webapiwithsqlserverdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private readonly DemoDbContext _demoDbContext;
+        public JobController(DemoDbContext demoDbContext)
+        {
+            _demoDbContext = demoDbContext;
+        }
+        //List
+        [HttpGet]
+        public async Task<IEnumerable<Job>> Get()
+
+            => await _demoDbContext.Jobs.ToListAsync();
+    }
+}
